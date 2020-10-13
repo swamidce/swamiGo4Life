8b201974248d112ae50dd4107ee28673843557be
SWAMI NANDAN <53258898+swamidce@users.noreply.github.com>
2020-10-13 16:33:17 +0530

    Update and rename sales.json to sales.js

---
+++ sales.js
@@ -0,0 +1,10 @@
+var sales = [
+  {
+    "CustomerId": "1",
+    "Date": "15/10/2020",
+    "Quantity": "1000.0",
+    "ProductName": "Fruits",
+    "PaymentMode": "Card",
+    "Discount": "20%"
+  }
+];
