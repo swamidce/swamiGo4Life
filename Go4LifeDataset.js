3eb92810f0b341e67b53a2eb8c8adbc6ae1e453d
SWAMI NANDAN <53258898+swamidce@users.noreply.github.com>
2020-10-13 16:43:04 +0530

    Create Go4LifeDataset.js

---
+++ Go4LifeDataset.js
@@ -0,0 +1,22 @@
+var sales = [
+  {
+    "CustomerId": "1",
+    "Date": "15/10/2020",
+    "Quantity": "1000.0",
+    "ProductName": "Fruits",
+    "PaymentMode": "Card",
+    "Discount": "20%"
+  }
+];
+var procurement = [
+  {
+    "Identifying": "NA",
+    "Finding": "NA",
+    "Requesting": "1",
+    "Negotiating": "NA",
+    "Agreeing": "NA",
+    "Arranging": "NA",
+    "Performing": "NA",
+    "Analysing": "NA"
+  }
+];
