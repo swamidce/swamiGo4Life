3d41f1c30a344e52c4899926b331c0140fd9f617
SWAMI NANDAN <53258898+swamidce@users.noreply.github.com>
2020-10-13 16:46:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-import React from 'react'
-import Features from 'src/components/home/Features'
-import SEO from 'src/components/seo'
-import Hero from 'src/components/home/hero'
-import DarkBlock from 'src/components/home/DarkBlock'
+// Require or import the dependencies
+const fs = require("fs");
+const sqlite3 = require("sqlite3").verbose();
 
-const IndexPage = () => (
-  <React.Fragment>
-    <SEO title='npm cli' />
-    <Hero />
-    <Features />
-    <DarkBlock />
-  </React.Fragment>
-)
+// Read the SQL file
+const dataSql = fs.readFileSync("./data.sql").toString();
 
-export default IndexPage
+// Setup the database connection
+let db = new sqlite3.Database("mydatabase", err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the in-memory SQLite database.");
+});
+
+// Convert the SQL string to array so that you can run them one at a time.
+// You can split the strings using the query delimiter i.e. `;` in // my case I used `);` because some data in the queries had `;`.
+const dataArr = dataSql.toString().split(");");
+
+// db.serialize ensures that your queries are one after the other depending on which one came first in your `dataArr`
+db.serialize(() => {
+  // db.run runs your SQL query against the DB
+  db.run("PRAGMA foreign_keys=OFF;");
+  db.run("BEGIN TRANSACTION;");
+  // Loop through the `dataArr` and db.run each query
+  dataArr.forEach(query => {
+    if (query) {
+      // Add the delimiter back to each query before you run them
+      // In my case the it was `);`
+      query += ");";
+      db.run(query, err => {
+        if (err) throw err;
+      });
+    }
+  });
+  db.run("COMMIT;");
+});
+
+// Close the DB connection
+db.close(err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Closed the database connection.");
+});
