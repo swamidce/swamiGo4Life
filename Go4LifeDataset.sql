78623f429ba0e2db36a366b160f1690a832db7c9
SWAMI NANDAN <53258898+swamidce@users.noreply.github.com>
2020-10-13 21:14:46 +0530

    Update Go4LifeDataset.sql

--- Go4LifeDataset.sql
+++ Go4LifeDataset.sql
@@ -12,7 +12,7 @@ INSERT INTO `sales` VALUES (1,'15/10/2020',1000.0,'Fruits','Card','20%');
 CREATE TABLE IF NOT EXISTS `procurement` (
 	`Identifying the needs of Goods and Services`	TEXT NOT NULL,
 	`Finding Suppliers`	TEXT NOT NULL,
-	`Requesting Proposals`	TEXT NOT NULL,
+	`Requesting Proposals`	VARCHAR(200) NOT NULL,
 	`Negotiating with Suppliers`	TEXT NOT NULL,
 	`Agreeing terms with Suppliers`	TEXT NOT NULL,
 	`Arranging and Receiving products and services`	TEXT NOT NULL,
